a9bf822de0f7e16b60f0976f810c922baf9bdd46
SiRyuA <siryua@develoid.com>
2015-10-13 22:07:25 +0900

    2DOF Add

--- 3DOF/arduino/hardware_arduino.ino
+++ 3DOF/arduino/hardware_arduino.ino
@@ -6,9 +6,20 @@
 #define M2_D 12
 #define M3_D 37
 
+unsigned long pwm_value1 = 0;
+unsigned long pwm_value2 = 0;
+
+int way = 0;
+int i = 0;
+int j = 0;
+ 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(A12, INPUT);
+  pinMode(A13, INPUT);
 
   pinMode(M1_U, OUTPUT);
   pinMode(M1_D, OUTPUT);
@@ -74,7 +85,7 @@ void loop() {
     }
 
     if(Read == 's') {
-      Serial.println("M S");
+      Serial.println("Motor Stop");
       digitalWrite(M1_U, LOW), digitalWrite(M1_D, LOW);
       digitalWrite(M2_U, LOW), digitalWrite(M2_D, LOW);
       digitalWrite(M3_U, LOW), digitalWrite(M3_D, LOW);
@@ -82,8 +93,45 @@ void loop() {
 
     if(Read == 'x') {
       Serial.flush();
+      i=0;
+      j=0;
     }
   }
-  delay(50);
+  delay(1);
   Serial.flush();
+
+  Analog_TEST();
+
 }
+
+void PWM_TEST(void) {
+  pwm_value1 = pulseIn(2, HIGH);
+  pwm_value2 = pulseIn(3, HIGH);
+
+  Serial.print("1 : ");
+  Serial.print(pwm_value1);
+  Serial.print("\t\t");
+  Serial.print("2 : ");
+  Serial.println(pwm_value2);  
+}
+
+void Analog_TEST(void) {    
+  int ARead1 = analogRead(A12);
+  int ARead2 = analogRead(A13);
+
+  if(ARead1 > 900) i++;
+  if(ARead2 > 900) j++; 
+
+  Serial.print("Hall A : ");
+  Serial.print(ARead1);
+  Serial.print("\t | ");
+  Serial.print("Hall B : ");
+  Serial.print(ARead2);
+  Serial.print("\t | ");
+  Serial.print("Pulse A HIGH : ");
+  Serial.print(i);
+  Serial.print("\t | ");
+  Serial.print("Pulse B HIGH : ");
+  Serial.println(j);
+}
+
