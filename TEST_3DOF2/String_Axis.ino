a525c87552b365bd777ac35e506d49cece5b0338
SiRyuA <siryua@develoid.com>
2015-11-02 02:47:31 +0900

    20151102 Update

---
+++ TEST_3DOF2/String_Axis.ino
@@ -0,0 +1,93 @@
+void stringMotorAxis() {
+  Serial.println("visaRead : " + visaRead);
+  
+  int comma1 = visaRead.indexOf(',');
+  int comma2 = visaRead.indexOf(',', comma1+1);
+  int comma3 = visaRead.indexOf(',', comma2+1);
+  int comma4 = visaRead.indexOf(',', comma3+1);
+  
+  String sTemp1 = visaRead.substring(0, comma1);
+  String sTemp2 = visaRead.substring(comma1+1,comma2);
+  String sTemp3 = visaRead.substring(comma2+1,comma3);
+  
+  Serial.println("sTemp1 : " + sTemp1 + "\t sTemp2 : " + sTemp2 + "\t sTemp3 : " + sTemp3);
+  
+  char cTemp1[sTemp1.length()+1];
+  char cTemp2[sTemp2.length()+1];
+  char cTemp3[sTemp3.length()+1];
+  
+  sTemp1.substring(0).toCharArray(cTemp1, sTemp1.length()+1);
+  sTemp2.substring(0).toCharArray(cTemp2, sTemp2.length()+1);
+  sTemp3.substring(0).toCharArray(cTemp3, sTemp3.length()+1);
+  
+  Serial.print("cTemp1 : ");Serial.print(cTemp1);Serial.print("\t");
+  Serial.print("cTemp2 : ");Serial.print(cTemp2);Serial.print("\t");
+  Serial.print("cTemp3 : ");Serial.print(cTemp3);Serial.print("\n");
+  
+  if((cTemp1[0] == 'L') || (cTemp1[0] == 'l')) {
+    Axis_Data[Motor_L] = strtol(cTemp1+1, NULL, 10);
+    Axis_Data[Motor_L] = map(constrain(Axis_Data[Motor_L], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("L : ");
+    Serial.print(Axis_Data[Motor_L]);
+    Serial.print("\t");
+  }
+  if((cTemp1[0] == 'M') || (cTemp1[0] == 'm')) {
+    Axis_Data[Motor_M] = strtol(cTemp1+1, NULL, 10);
+    Axis_Data[Motor_M] = map(constrain(Axis_Data[Motor_M], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("M : ");
+    Serial.print(Axis_Data[Motor_M]);
+    Serial.print("\t");
+  }
+  if((cTemp1[0] == 'R') || (cTemp1[0] == 'r')) {
+    Axis_Data[Motor_R] = strtol(cTemp1+1, NULL, 10);
+    Axis_Data[Motor_R] = map(constrain(Axis_Data[Motor_R], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("R : ");
+    Serial.print(Axis_Data[Motor_R]);
+    Serial.print("\t");
+  }
+  
+  if((cTemp2[0] == 'L') || (cTemp2[0] == 'l')) {
+    Axis_Data[Motor_L] = strtol(cTemp2+1, NULL, 10);
+    Axis_Data[Motor_L] = map(constrain(Axis_Data[Motor_L], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("L : ");
+    Serial.print(Axis_Data[Motor_L]);
+    Serial.print("\t");
+  }
+  if((cTemp2[0] == 'M') || (cTemp2[0] == 'm')) {
+    Axis_Data[Motor_M] = strtol(cTemp2+1, NULL, 10);
+    Axis_Data[Motor_M] = map(constrain(Axis_Data[Motor_M], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("M : ");
+    Serial.print(Axis_Data[Motor_M]);
+    Serial.print("\t");
+  }
+  if((cTemp2[0] == 'R') || (cTemp2[0] == 'r')) {
+    Axis_Data[Motor_R] = strtol(cTemp2+1, NULL, 10);
+    Axis_Data[Motor_R] = map(constrain(Axis_Data[Motor_R], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("R : ");
+    Serial.print(Axis_Data[Motor_R]);
+    Serial.print("\t");
+  }
+  
+  if((cTemp3[0] == 'L') || (cTemp3[0] == 'l')) {
+    Axis_Data[Motor_L] = strtol(cTemp3+1, NULL, 10);
+    Axis_Data[Motor_L] = map(constrain(Axis_Data[Motor_L], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("L : ");
+    Serial.print(Axis_Data[Motor_L]);
+    Serial.print("\n");
+  }
+  if((cTemp3[0] == 'M') || (cTemp3[0] == 'm')) {
+    Axis_Data[Motor_M] = strtol(cTemp3+1, NULL, 10);
+    Axis_Data[Motor_M] = map(constrain(Axis_Data[Motor_M], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("M : ");
+    Serial.print(Axis_Data[Motor_M]);
+    Serial.print("\n");
+  }
+  if((cTemp3[0] == 'R') || (cTemp3[0] == 'r')) {
+    Axis_Data[Motor_R] = strtol(cTemp3+1, NULL, 10);
+    Axis_Data[Motor_R] = map(constrain(Axis_Data[Motor_R], Min, Axis_Max), Min, Axis_Max, Min, Motor_Max);
+    Serial.print("R : ");
+    Serial.print(Axis_Data[Motor_R]);
+    Serial.print("\n");
+  }
+}
+
---
+++ TEST_3DOF2/TEST_3DOF2.ino
@@ -0,0 +1,67 @@
+#define Motor_L 0
+#define Motor_M 1
+#define Motor_R 2
+
+#define delay_time 20
+
+#define Min 0
+#define Axis_Max 255
+#define Motor_Max 80
+
+int Motor_CW[3] = {22,28,36}; 
+int Motor_CCW[3] = {27,12,37}; 
+int Encoder[3] = {2,3,18}; 
+int Tone = 50;
+
+volatile int Encoder_Data[3] = {0,0,0};
+volatile int Encoder_Save[3] = {0,0,0};
+
+int Motor_work[3] = {0,0,0};
+int Motor_way[3] = {0,0,0};
+int Axis_Data[3] = {0,0,0};
+
+String visaRead = "";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+
+  Serial.println("contacted.");
+
+  for(char i=0; i<3; i++) { 
+   pinMode(Motor_CW[i], OUTPUT); 
+   pinMode(Motor_CCW[i], OUTPUT); 
+   pinMode(Encoder[i], INPUT); 
+  } 
+
+  Serial.println("settings."); 
+  Motor_START(); 
+ 
+  Serial.println("start."); 
+  Serial.flush(); 
+} 
+
+void loop() {
+  while (Serial.available() > 0) {
+    //visaRead = Serial.readString();
+    visaRead = Serial.readStringUntil(';');
+
+    Serial.println(visaRead);
+
+    if(visaRead != "") {
+      if((visaRead == "U") || (visaRead == "u") || (visaRead == "D") || (visaRead == "d") || (visaRead == "S") || (visaRead == "s") || (visaRead == "X") || (visaRead == "x")) {
+        if((visaRead == "U") || (visaRead == "u")) Motor_FULLUP();
+        if((visaRead == "D") || (visaRead == "d")) Motor_FULLDW();
+        if((visaRead == "S") || (visaRead == "s")) Motor_START();
+        if((visaRead == "X") || (visaRead == "x")) Motor_RST();
+      }else stringMotorAxis();
+    }
+    Serial.println("\n");
+  }
+  Motor_WORK();
+  
+  visaRead = "";
+  Serial.flush();
+  delay(delay_time);
+}
+
